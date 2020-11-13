85b4978c158cfb78d4c82c49e611af650066b049
AJILEYE AYOKUNLE J <kunlesymls@hotmail.com>
2020-11-13 13:09:09 +0100

    Adding invoicing demo to the project

--- Plugin.Monnify.Demo/Controllers/Payments.cs
+++ Plugin.Monnify.Demo/Controllers/Payments.cs
@@ -32,30 +32,53 @@ namespace Plugin.Monnify.Demo.Controllers
                 CustomerEmail = "kunlesymls@gmail.com", // my real email... Feel free to contact me
                 CustomerPhoneNumber = "07036927669", // my real phone number... Feel free to contact me
                 PaymentReference = Guid.NewGuid().ToString(),
-                PaymentDescription = "Testig one time payment",
+                PaymentDescription = "Testing one time payment",
                 CurrencyCode = "NGN",
                 ContractCode = _monnifyClient.ContractCode,
                 //RedirectUrl = "https://localhost:44389/Payments/ConfirmPayment",
-                RedirectUrl = Url.Action("ConfirmPayment", "Payments"),
+                RedirectUrl = $"https://{Request.Host.Value}{Url.Action("ConfirmPayment", "Payments")}",
                 IncomeSplitConfig = new List<Incomesplitconfig>()
             });
             return View(oneTimePaymentApiResponse);
         }
 
 
+        public async Task<IActionResult> CreateInvoice()
+        {
+            var invoiceResponse = await _monnifyClient.CreateInvoice(new InvoiceRequest
+            {
+                Amount = 500,
+                ContractCode = _monnifyClient.ContractCode,
+                CurrencyCode = "NGN",
+                ExpiryDate = DateTime.Now.AddDays(2).ToString("yyyy-MM-dd HH:mm:ss"),
+                CustomerEmail = "kunlesymls@gmail.com",
+                CustomerName = "Joseph Ajileye",
+                Description = $"Wallet Top-up for Joseph Ajileye",
+                InvoiceReference = Guid.NewGuid().ToString(),
+                RedirectUrl = $"https://{Request.Host.Value}{Url.Action("ConfirmPayment", "Payments")}",
+                //AccountReference = customer.CustomerWallet.MonnifyAccountReference
+            });
+            return View(invoiceResponse);
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> ConfirmPayment(string paymentReference)
         {
+            var paymentDetail = await _monnifyClient.VerifyTransactionByPaymentReference(
+                                        new TransactionStatusRequest { PaymentReference = paymentReference });
 
-            //Verify Payment Status by calling back to Monnify
-            var response = await _monnifyClient.VerifyTransactionByPaymentReference(new TransactionStatusRequest { PaymentReference = paymentReference });
-            if (response.ResponseBody.PaymentStatus.Trim().ToLower() == "paid")
+            ////Verify Payment Status by calling back to Monnify
+            //var response = await _monnifyClient.VerifyTransactionByTransactionReference(new TransactionStatusRequest
+            //                                        { TransactionReference = paymentDetail.ResponseBody.TransactionReference });
+
+            if (paymentDetail.ResponseBody.PaymentStatus.Trim().ToLower() == "paid")
             {
                 //persist and log successful response 
-                return View(response);
+                return View(paymentDetail);
             }
 
-            return View(response);
+            return View(paymentDetail);
         }
     }
 }
